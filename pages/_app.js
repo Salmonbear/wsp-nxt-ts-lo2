1bdb9cba5d69e8b16b3cf0b61360ff8cf36f5656
Salmonbear <si_bruce@hotmail.com>
2024-08-11 16:26:18 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -47,6 +47,16 @@ export default function CustomApp({ Component, pageProps }) {
           }}
         />
       </Head>
+          <Script
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-XXXXXXX');`
+        }}
+      />
       <Component {...pageProps} />
     </>
   );
